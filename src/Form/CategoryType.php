cdf3ac870d856f2badb447d882601e87afed7c49
Dmitry Zraiev <dmitryzrayev@gmail.com>
2018-12-14 17:28:49 +0200

    add php-cs-fixer

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use App\Form\Type\WorkflowType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('slug', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('body', TextareaType::class)
+            ->add('image', FileType::class)
+            ->add('status', WorkflowType::class, [
+                'placeholder' => 'Choose a article status option', ])
+            ->add('save', SubmitType::class, ['label' => 'Create Post', 'attr' => ['class' => 'btn btn-default pull-right']])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
