68db7cceecaeae5bc170a64adcd50e6c290e0e6e
zrayev <dmitryzrayev@gmail.com>
2018-12-24 00:46:21 +0200

    add knp menu

---
+++ src/Menu/Builder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class Builder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * @param RequestStack $requestStack
+     *
+     * @return ItemInterface
+     */
+    public function MyMenu(RequestStack $requestStack)
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'navbar-nav mr-auto');
+        $menu->addChild('Blog', ['route' => 'blog']);
+        $menu->addChild('Categories', ['route' => 'categories']);
+        $menu->addChild('Tags', ['route' => 'tags']);
+        $menu->addChild('Authors', ['route' => 'authors']);
+
+        foreach ($menu as $child) {
+            $child->setLinkAttribute('class', 'nav-link')
+                ->setAttribute('class', 'nav-item');
+        }
+
+        return $menu;
+    }
+}
