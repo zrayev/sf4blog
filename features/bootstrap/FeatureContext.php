82b179ab766481dfd2fd49622e848268e062878a
zrayev <dmitryzrayev@gmail.com>
2019-03-03 20:08:05 +0200

    add tests

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Entity\User;
 use Behat\Behat\Context\Context;
+use Behat\Behat\Context\Environment\InitializedContextEnvironment;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behatch\Context\RestContext;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * This context class contains the definitions of the steps used by the demo
@@ -19,13 +25,30 @@ class FeatureContext implements Context
     private $kernel;
 
     /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var JWTTokenManagerInterface
+     */
+    private $jwtManager;
+
+    /**
+     * @var RestContext
+     */
+    private $restContext;
+
+    /**
      * @var Response|null
      */
     private $response;
 
-    public function __construct(KernelInterface $kernel)
+    public function __construct(KernelInterface $kernel, EntityManagerInterface $em, JWTTokenManagerInterface $jwtManager)
     {
         $this->kernel = $kernel;
+        $this->em = $em;
+        $this->jwtManager = $jwtManager;
     }
 
     /**
@@ -44,5 +67,32 @@ class FeatureContext implements Context
         if ($this->response === null) {
             throw new \RuntimeException('No response received');
         }
+    }
+
+    /**
+     * @BeforeScenario @login
+     *
+     * @param BeforeScenarioScope $scope
+     */
+    public function login(BeforeScenarioScope $scope)
+    {
+        $user = $this->findUser('admin');
+        /** @var User $user */
+        $token = $this->jwtManager->create($user);
+        /** @var InitializedContextEnvironment $environment */
+        $environment = $scope->getEnvironment();
+        $this->restContext = $environment->getContext(RestContext::class);
+        $this->restContext->iAddHeaderEqualTo('Authorization', "Bearer $token");
+    }
+
+    /**
+     * @param $username
+     * @return User|null|object
+     */
+    private function findUser($username)
+    {
+        $repository = $this->em->getRepository(User::class);
+
+        return $repository->findBy(['username' => $username]);
     }
 }
