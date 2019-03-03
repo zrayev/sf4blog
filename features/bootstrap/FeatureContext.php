771f0f41b2517f8fd2443f64114188173ada7634
zrayev <dmitryzrayev@gmail.com>
2019-03-03 18:14:26 +0200

    install Behat Symfony, Mink

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,22 +1,48 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
- * Defines application features from the specific context.
+ * This context class contains the definitions of the steps used by the demo
+ * feature file. Learn how to get started with Behat and BDD on Behat's website.
+ *
+ * @see http://behat.org/en/latest/quick_start.html
  */
 class FeatureContext implements Context
 {
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * @var KernelInterface
      */
-    public function __construct()
+    private $kernel;
+
+    /**
+     * @var Response|null
+     */
+    private $response;
+
+    public function __construct(KernelInterface $kernel)
     {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @When a demo scenario sends a request to :path
+     */
+    public function aDemoScenarioSendsARequestTo(string $path)
+    {
+        $this->response = $this->kernel->handle(Request::create($path, 'GET'));
+    }
+
+    /**
+     * @Then the response should be received
+     */
+    public function theResponseShouldBeReceived()
+    {
+        if ($this->response === null) {
+            throw new \RuntimeException('No response received');
+        }
     }
 }
