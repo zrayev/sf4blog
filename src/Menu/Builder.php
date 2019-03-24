7dd720f142aac8042b05e9e11d0f7656dbeed158
zrayev <dmitryzrayev@gmail.com>
2019-03-24 20:14:01 +0200

    minore fix

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -64,7 +64,7 @@ class Builder
         if ($this->authorizationChecker->isGranted(['ROLE_ADMIN'])) {
             $menu->addChild('Admin', ['route' => 'sonata_admin_redirect', 'label' => 'Admin']);
         }
-        if ($this->authorizationChecker->isGranted(['``'])) {
+        if ($this->authorizationChecker->isGranted(['ROLE_USER'])) {
             $menu->addChild('Logout', ['route' => 'logout', 'label' => 'Logout']);
         } else {
             $menu->addChild('Login', ['route' => 'app_login', 'label' => 'Login']);
